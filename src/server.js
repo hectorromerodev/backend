aeb557346bf4baa982ab5428fe0d4402b4015acb
Hector A. Romero Morales <harm0294@GMAIL.COM>
2019-12-03 19:02:58 -0800

    server config

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 //Dependencias
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+//Port settings
+const port = process.env.PORT || 4232;
 //Start our app
 const app = express();
-
-app.listen(3000);
-console.log("Server on port: ", 3000);
+//App middleware settings
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(port, function() {
+	console.log(`Server on port: ${port}`);
+});
